254d659f88ad6918791e4c29c3a8694bf03da6e5
Abel Arismendy <abel@arismendy.co>
2024-04-24 19:43:12 -0500

    fix tailwind css config 3.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,17 +2,13 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false,
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   theme: {
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
     },
-  },
-  variants: {
-    extend: {},
   },
   plugins: [
     require('daisyui'),
