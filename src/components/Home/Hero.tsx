3e46ea1ba2ed7b525bc2d149bf133fac026a8aeb
Abel Arismendy <abel@arismendy.co>
2024-04-26 23:07:15 -0500

    navbar, placeholder hero

--- src/components/Home/Hero.tsx
+++ src/components/Home/Hero.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
 const Hero: React.FC = () => {
+    // Reemplaza el contenido de este componente, la imagen es solo temporal para probar el navbar
     return (
-        <div className="hero">
+        
+        <div className="hero h-screen" style={{ backgroundImage: 'url("https://source.unsplash.com/1600x900/?nature,water")'}}>
             <h1>Welcome to the Hero component!</h1>
         </div>
     );
--- src/components/layout/NavBar.tsx
+++ src/components/layout/NavBar.tsx
@@ -4,10 +4,10 @@ import logo from '@assets/img/OASIS-LOGO_isologo-gradient-white-07.svg';
 
 const NavBar: React.FC = () => {
     return (
-        <div className="drawer">
+        <div className="drawer mb-16 lg:mb-[84px]">
         <input id="my-drawer-3" type="checkbox" className="drawer-toggle" /> 
         <div className="drawer-content flex flex-col">
-            <div className="w-full navbar glass rounded-3xl">
+            <div className="w-full max-w-screen-xl navbar glass rounded-3xl fixed top-0 left-[50%] transform translate-x-[-50%] lg:mt-2">
                 <div className="flex-none lg:hidden">
                     <label htmlFor="my-drawer-3" aria-label="open sidebar" className="btn btn-square btn-ghost">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
@@ -20,7 +20,7 @@ const NavBar: React.FC = () => {
                 </div>
                 <div className="flex-none hidden lg:block">
                     <ul className="menu menu-horizontal">
-                    <li><NavLink to="/" >Inicio</NavLink></li>
+                    <li><NavLink to="/">Inicio</NavLink></li>
                     <li><NavLink to="/nosotros">Nosotros</NavLink></li>
                     </ul>
                 </div>
@@ -28,7 +28,7 @@ const NavBar: React.FC = () => {
         </div> 
         <div className="drawer-side">
             <label htmlFor="my-drawer-3" aria-label="close sidebar" className="drawer-overlay"></label> 
-            <ul className="menu p-4 w-80 min-h-full bg-base-200">
+            <ul className="menu p-4 w-80 min-h-full bg-base-200 glass">
             <li><NavLink to="/">Inicio</NavLink></li>
             <li><NavLink to="/nosotros">Nosotros</NavLink></li>
             </ul>
