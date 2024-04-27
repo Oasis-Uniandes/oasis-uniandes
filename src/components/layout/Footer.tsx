577302896659481593c0fe1bcbea9bec27939daf
Abel Arismendy <abel@arismendy.co>
2024-04-26 21:59:50 -0500

    Footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,10 +1,33 @@
 import React from 'react';
+import logo from '@assets/img/OASIS-LOGO_imagotipo-gradient-white.svg';
+import { IconBrandInstagram, IconBrandTiktok, IconBrandLinkedin } from '@tabler/icons-react';
+import { Link } from 'react-router-dom';
 
 const Footer: React.FC = () => {
     return (
-        <footer>
-            <h1>Footer</h1>
-            {/* Add your footer content here */}
+        <footer className="footer footer-center p-10 bg-secondary text-white">
+        <aside>
+            <Link to="/">
+            <img src={logo} alt="Oasis Logo" className="w-32 h-32" />
+            </Link>
+            <p>
+            Impulsamos tu aprendizaje en tecnologías de <b>Realidad Extendida</b>
+            </p> 
+            <p className='text-primary'>Hecho con ❤️ por el <Link to="/nosotros" className='font-bold hover:text-accent active:text-white'>equipo Oasis</Link></p>
+        </aside>
+        <nav>
+            <div className="grid grid-flow-col gap-4">
+            <a href="https://www.instagram.com/oasis.uniandes/" target="_blank" rel="noreferrer" className='hover:text-primary active:text-accent'>
+                <IconBrandInstagram size={32} />
+            </a>
+            <a href="https://www.tiktok.com/@oasis.uniandes" target="_blank" rel="noreferrer" className='hover:text-primary active:text-accent'>
+                <IconBrandTiktok size={32} />
+            </a>
+            <a href="https://www.linkedin.com/company/oasis-uniandes/" target="_blank" rel="noreferrer" className='hover:text-primary active:text-accent'>
+                <IconBrandLinkedin size={32} />
+            </a>
+            </div>
+        </nav>
         </footer>
     );
 };
