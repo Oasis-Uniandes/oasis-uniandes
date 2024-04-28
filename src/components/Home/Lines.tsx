0f13c0da8c1c79191e03a303169b2aed2f4fa470
Abel Arismendy <abel@arismendy.co>
2024-04-28 09:11:27 -0500

    base

--- src/components/Home/Lines.tsx
+++ src/components/Home/Lines.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Lines: React.FC = () => {
     return (
-        <div>
-            <h1>Lineas de Impacto</h1>
-            {/* Your code for the "Lines" component goes here */}
-        </div>
+        <section className="bg-secondary w-full rounded-3xl">
+            <div className='container flex flex-col items-center gap-12 py-24'>
+                <div className="prose !prose-invert">
+                    <h1>Lineas de Acción</h1>
+                </div>
+                <div>
+                    <Link to="/" className='mask mask-circle'>
+                        <figure className="relative">
+                            <img src="https://via.placeholder.com/300" alt="Linea de Acción"/>
+                            <figcaption className="absolute inset-0 flex items-center justify-center text-white text-lg font-semibold">
+                                Talleres de Desarrollo
+                            </figcaption>
+                        </figure>
+                    </Link>
+                </div>
+            </div>
+        </section>
     );
 };
 
