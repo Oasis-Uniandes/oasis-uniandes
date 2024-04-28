265dfbac152f219f162640369194c584836df48e
Abel Arismendy <abel@arismendy.co>
2024-04-28 08:43:28 -0500

    improve Partners

--- src/components/Home/Partners.tsx
+++ src/components/Home/Partners.tsx
@@ -8,8 +8,10 @@ import logoDisc from '@assets/img/logos/DISC.png';
 const Partners: React.FC = () => {
     return (
         <section className='container flex flex-col items-center gap-12 py-24'>
-            <article className='prose'>
-                <h1>Agradecimientos</h1>
+            <article className='prose max-w-none w-full'>
+                <h3 className='text-left md:text-center'>
+                Oasis es posible gracias al apoyo de nuestros valiosos colaboradores.
+                </h3>
             </article>
             <ul className='grid grid-cols-1 items-center gap-x-8 gap-y-10 sm:grid-cols-2 sm:gap-x-10 lg:mx-0 lg:grid-cols-4 justify-items-center'>
                 <li>
