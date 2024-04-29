0595fc5f31fbce346a35d6e7c33f944c816923f7
Abel Arismendy <abel@arismendy.co>
2024-04-28 21:53:20 -0500

    minor fixes

--- src/components/About/Title.tsx
+++ src/components/About/Title.tsx
@@ -2,7 +2,7 @@ import teamImg from '@assets/img/team.jpg';
 
 const Title: React.FC = () => {
     return (
-        <div className="hero h-dvh rounded-3xl" style={{backgroundImage: `url(${teamImg})`}}>
+        <div className="hero h-[500px] sm:h-svh rounded-3xl" style={{backgroundImage: `url(${teamImg})`}}>
             <div className="hero-overlay bg-gradient-to-t from-secondary to-transparent rounded-3xl"></div>
             <div className="text-center prose !prose-invert flex flex-col justify-end h-full py-20 items-center">
                 <h1 className='text-5xl mb-8'>¿Quienes Somos?</h1>
