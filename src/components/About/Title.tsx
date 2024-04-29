829e83e5ae847808596550990d025072dd494ea8
Abel Arismendy <abel@arismendy.co>
2024-04-28 19:23:26 -0500

    title about

--- src/components/About/Title.tsx
+++ src/components/About/Title.tsx
@@ -1,11 +1,14 @@
+import teamImg from '@assets/img/team.jpg';
+
 const Title = () => {
     return (
-        <div className="bg-secondary w-full rounded-3xl">
-        <div className="container gap-12 py-24">
-            <div className="prose !prose-invert max-w-none">
-                <h1 className="text-center">Quienes Somos</h1>
+        <div className="hero sm:min-h-[600px] rounded-3xl" style={{backgroundImage: `url(${teamImg})`}}>
+            <div className="hero-overlay bg-gradient-to-t from-secondary to-transparent rounded-3xl"></div>
+            <div className="text-center prose !prose-invert flex flex-col justify-end h-full py-20 items-center">
+                <h1 className='text-5xl'>¿Quienes Somos?</h1>
+                <p className="not-prose px-4">Somos un grupo de estudiantes de la Universidad de los Andes apasionados por las tecnologías de Realidad Extendida.</p>
+                <button className='btn btn-accent btn-outline w-48 mt-6'>Únete a Nosotros</button>
             </div>
-        </div>
         </div>
     );
 }
