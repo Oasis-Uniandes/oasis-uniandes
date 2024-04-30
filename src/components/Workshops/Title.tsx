13b860ebf0d1d49245c0b4a2c9ff1e4376101614
Abel Arismendy <abel@arismendy.co>
2024-04-30 17:50:19 -0500

    fix talleres image

--- src/components/Workshops/Title.tsx
+++ src/components/Workshops/Title.tsx
@@ -2,7 +2,7 @@ import HeroTitle from '@layout/HeroTitle'
 
 const Title: React.FC = () => { 
     return (
-        <HeroTitle title='Talleres de Desarrollo' buttonText='Quiero ser Aprendiz!' buttonLink='/' image='talleres.jpg'>
+        <HeroTitle title='Talleres de Desarrollo' buttonText='Quiero ser Aprendiz!' buttonLink='/' image='talleres_hero.jpg'>
                     <p>Realizamos talleres de desarrollo de aplicaciones de <b>Realidad Extendida</b> con nuestros <b>Aprendices</b>.</p>
         </HeroTitle>
     );
