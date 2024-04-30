afc1cf1be75505cd25cc4138acd055ab07440fa8
Abel Arismendy <arismendyc.design@gmail.com>
2024-04-30 11:13:58 -0500

    members animating

--- src/components/About/Leaders.tsx
+++ src/components/About/Leaders.tsx
@@ -1,15 +1,20 @@
 import PeopleCard from "./PeopleCard";
 
+import { Reveal, RevealGroup } from '@utils/Reveal/index'; 
 
 const Leaders: React.FC = () => {
     return (
         <div className="container pt-8 flex gap-8 gap-x-16 flex-wrap justify-center">
-            <div className="divider text-3xl w-full">Líderes</div>
+            <Reveal className="divider text-3xl w-full" delay={0.2} easing="easeOut">
+            <span>Líderes</span>
+            </Reveal>
+            <RevealGroup className="flex gap-8 gap-x-16 flex-wrap justify-center" delay={0.1} duration={0.35} easing="easeOut">
             <PeopleCard size="lg" name="Angel Abel Arismendy" title="Ingeniería de Sistemas" roles={["Líder",  "Monitor de Colivri", "Desarrollador Web", "Diseñador gráfico"]} image="abel.png" url="https://abel.arismendy.co"
             ring/>
             <PeopleCard size="lg" name="Juan Esteban Rodriguez" title="Ingeniería de Sistemas" roles={["Líder", "Monitor de Vivero Virtual"]} image="juan.png" ring/>
             <PeopleCard size="lg" name="Juan José Sierra" title="Ingeniería de Sistemas" roles={["Líder", "Monitor de Videojuegos"]} image="juanjo.jpg" ring/>
             <PeopleCard size="lg" name="Julian Mora" title="Maestría en Ingeniería de Sistemas" roles={["Líder", "Asistente Graduado"]} image="julian.png" ring/>
+            </RevealGroup>
         </div>
     )
 }
--- src/components/About/Team.tsx
+++ src/components/About/Team.tsx
@@ -1,16 +1,20 @@
 import PeopleCard from "./PeopleCard";
-
+import { Reveal, RevealGroup } from '@utils/Reveal/index';
 import teamData from '@data/team.json'; 
 
 
 const Team: React.FC = () => {
     teamData.sort((a, b) => a.name.localeCompare(b.name));
     return (
-        <div className="container pt-8 flex gap-8 gap-x-16 flex-wrap justify-center">
-            <div className="divider text-3xl w-full">Equipo</div>
+        <div className="container pt-8 flex gap-8 flex-wrap justify-center">
+            <Reveal className="divider text-3xl w-full" delay={0.2} easing="easeOut">
+            <span>Equipo</span>
+            </Reveal>
+            <RevealGroup className="flex gap-8 gap-x-16 flex-wrap justify-center" delay={0.1} duration={0.2} easing="easeOut">
             {teamData.map((person, index) => (
                 <PeopleCard key={index} size="md" name={person.name} title={person.title} roles={person.roles} image={person.image} url={person.url}/>
             ))}
+            </RevealGroup>
         </div>
     )
 }
