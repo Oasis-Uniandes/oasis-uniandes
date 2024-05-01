5b7920cf5131bfa7abb297d3d37b8ca980d1bf02
Abel Arismendy <abel@arismendy.co>
2024-04-30 20:36:49 -0500

    carousel

--- src/components/Workshops/Carousel.tsx
+++ src/components/Workshops/Carousel.tsx
@@ -1,29 +1,54 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { IconChevronLeft, IconChevronRight } from '@tabler/icons-react';
 
 const Carousel: React.FC = () => {
-    return (
-        <div className="container carousel carousel-center p-4 space-x-4 bg-secondary rounded-3xl">
-            <div className="carousel-item">
-                <img src="https://daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.jpg" className="rounded-box" />
-            </div> 
-            <div className="carousel-item">
-                <img src="https://daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.jpg" className="rounded-box" />
-            </div> 
-            <div className="carousel-item">
-                <img src="https://daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.jpg" className="rounded-box" />
-            </div> 
-            <div className="carousel-item">
-                <img src="https://daisyui.com/images/stock/photo-1494253109108-2e30c049369b.jpg" className="rounded-box" />
-            </div> 
-            <div className="carousel-item">
-                <img src="https://daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.jpg" className="rounded-box" />
-            </div> 
-            <div className="carousel-item">
-                <img src="https://daisyui.com/images/stock/photo-1559181567-c3190ca9959b.jpg" className="rounded-box" />
-            </div> 
-            <div className="carousel-item">
-                <img src="https://daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.jpg" className="rounded-box" />
+    const [scrollX, setScrollX] = useState(0);
+
+    const imageWidth = 500;
+
+    const handleScroll = (direction: string) => {
+        const carousel = document.querySelector('.carousel');
+        const carouselWidth = carousel?.scrollWidth || 0;
+        const containerWidth = carousel?.clientWidth || 0;
+        const maxScrollX = carouselWidth - containerWidth;
+
+        if (direction === 'left') {
+            setScrollX(prev => Math.max(prev - imageWidth, 0));
+        } else {
+            setScrollX(prev => Math.min(prev + imageWidth, maxScrollX));
+        }
+    }
+
+    useEffect(() => {
+        const carousel = document.querySelector('.carousel');
+        carousel?.scrollTo({ left: scrollX, behavior: 'smooth' });
+    }, [scrollX]);
+
+        return (
+        <div className="container flex py-8">
+            <button className="prev" onClick={() => handleScroll('left')}><IconChevronLeft/></button>
+            <div className="carousel space-x-4 rounded-3xl">
+                <div className="carousel-item">
+                    <img src={`https://via.placeholder.com/${imageWidth}`} alt="Placeholder" className='rounded-3xl'/>
+                </div>
+                <div className="carousel-item">
+                    <img src={`https://via.placeholder.com/${imageWidth}`} alt="Placeholder" className='rounded-3xl'/>
+                </div>
+                <div className="carousel-item">
+                    <img src={`https://via.placeholder.com/${imageWidth}`} alt="Placeholder" className='rounded-3xl'/>
+                </div>
+                <div className="carousel-item">
+                    <img src={`https://via.placeholder.com/${imageWidth}`} alt="Placeholder" className='rounded-3xl'/>
+                </div>
+                <div className="carousel-item">
+                    <img src={`https://via.placeholder.com/${imageWidth}`} alt="Placeholder" className='rounded-3xl'/>
+                </div>
+                <div className="carousel-item">
+                    <img src={`https://via.placeholder.com/${imageWidth}`} alt="Placeholder" className='rounded-3xl'/>
+                </div>
+
             </div>
+            <button className="next" onClick={() => handleScroll('right')}><IconChevronRight/></button>
         </div>
     );
 }
